bcdd93f859b3e94b3fc2f2e8c980f259b4076544
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-02-22 16:11:05 +0100

    🐛 Fix submodule

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,10 +1,5 @@
 print("Hello from nvim/after/plugin/colorscheme.lua")
-local setup, _ = pcall(require, "vim-nightfly-colors")
-if not setup then
-    print("Failed to load vim-nightfly-colors.")
-end
-
 local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 if not status then
-    print("Failed to set the colorscheme.")
+    print("Colorscheme not found!")
 end
