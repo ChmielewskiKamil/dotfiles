6c8a4f9ff17e7573a074ffc5441f72cab0fe78f0
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-02-22 15:50:01 +0100

    👷 Experiment

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,5 +1,10 @@
 print("Hello from nvim/after/plugin/colorscheme.lua")
+local setup, _ = pcall(require, "vim-nightfly-colors")
+if not setup then
+    print("Failed to load vim-nightfly-colors.")
+end
+
 local status, _ = pcall(vim.cmd, "colorscheme nightfly")
 if not status then
-    print("Colorscheme not found!")
+    print("Failed to set the colorscheme.")
 end
