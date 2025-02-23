3f2562f39d9927ba218cdc849285dafc487e1a27
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-02-23 13:57:08 +0100

    👷 Add colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,5 +1,12 @@
 print("Hello from nvim/after/plugin/colorscheme.lua")
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+
+local status, _ = pcall(function ()
+    vim.cmd.colorscheme("nightfly")
+
+    -- transparent background
+    -- vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "normalFloat", { bg = "none" })
+end)
 if not status then
     print("Colorscheme not found!")
 end
