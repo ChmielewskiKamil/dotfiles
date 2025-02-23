aa99a88c218e0dfadfe2507246d296052154a3be
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-02-23 17:01:59 +0100

    ♻️ Separate options

---
+++ nvim/lua/kchmielewski/options.lua
@@ -0,0 +1,58 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation & tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Code folding
+vim.opt.foldmethod = "indent"
+vim.opt.foldnestmax = 2
+vim.opt.foldtext = "."
+
+-- Line wrapping
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- The Undotree gets access to the long term version history
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Searching
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Appearance
+vim.opt.termguicolors = true
+
+-- Leave 8 lines to eol at all times
+vim.opt.scrolloff = 8
+
+-- Diagnostics
+vim.opt.signcolumn = "yes"
+
+-- Additional chars in filenames
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+-- Visual line at 80 chars
+vim.opt.colorcolumn = "80,120"
+
+-- Hide command line unless needed
+vim.opt.cmdheight = 0
+
+-- Insert comment leader on new line via <CR> or o
+vim.opt.formatoptions = "jcroql"
+
+-- Use the same clipboard for system and nvim.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
