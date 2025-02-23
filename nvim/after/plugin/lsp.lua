f7a6e164668fca16ca405e32292bea0212447f21
Kamil Chmielewski <45183584+ChmielewskiKamil@users.noreply.github.com>
2025-02-23 19:16:12 +0100

    👷 Add basic language servers

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,11 @@
-return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function ()
-
-        end,
+require('mason').setup()
+require('mason-lspconfig').setup {
+    ensure_installed = {
+        'lua_ls',
+        'rust_analyzer',
+        'gopls',
+        'nomicfoundation-solidity-language-server'
     }
 }
+
+local lspconfig = require('lspconfig')
